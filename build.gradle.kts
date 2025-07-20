b94c00828309d48bf767321ab6c4d6b80011a4be
Florian Spieß <business@minn.dev>
2025-07-20 17:09:23 +0200

    Update JDA to 6.0.0-rc.1 (#3)

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,22 +3,15 @@ plugins {
 }
 
 group = "net.dv8tion.pokedex"
-version = "1.0-SNAPSHOT"
+version = "1.0"
 
 repositories {
     mavenCentral()
-
-    // We are getting the pre-release version from jitpack.
-    maven("https://jitpack.io")
 }
 
 dependencies {
-    // We are using a pre-release version of JDA as components haven't merged
-    // into main-line JDA yet. PR: https://github.com/discord-jda/JDA/pull/2809
-    implementation("io.github.freya022:JDA:c6f764778d")
-
-    // Once the above PR is merged and JDA does a release, you can replace the above implementation(...) with:
-    // implementation("net.dv8tion:JDA:$VERSION") where $VERSION is the new release
+    // Note: Remove "-rc.1" when 6.0.0 is released
+    implementation("net.dv8tion:JDA:6.0.0-rc.1")
 }
 
 tasks.test {
